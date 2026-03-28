bd293fc0f7da97d7699a749644979089de00050e
mahtsales <davimatheusfsales@gmail.com>
2026-03-28 09:49:36 -0400

    feat and fix: add list of exercise and edit archive name - exercise-01.py

---
+++ exercise-03.py
@@ -0,0 +1,6 @@
+#realizar a remocao do nome 'Bianca' da lista abaixo:
+peoples = ['Bianca', 'Alvaro', 'Enzo', 'Davi', 'Bianca', 'Bianca']
+for nome in peoples [:]: #vai removendo de maneira gradativa
+    if nome == 'Bianca':
+        peoples.remove(nome)
+        print(peoples)
--- exercise02.py
+++ exercise02.py
@@ -2,3 +2,4 @@ pessoas = ['Bianca', 'Alvaro', 'Enzo', 'Davi', 'Bianca', 'Bianca']
 for i in pessoas:
     pessoas.remove('Bianca')
 print(pessoas)  # Saída: ['Alvaro', 'Enzo', 'Davi']
+
