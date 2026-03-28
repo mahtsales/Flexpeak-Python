031ed1568a68082cc3927165af24fd1097ef9059
mahtsales <davimatheusfsales@gmail.com>
2026-03-28 11:51:11 -0400

    feat: add set list dictionary exercise

---
+++ class-2026-03-28/set-list-dictionary/peoples.py
@@ -0,0 +1,40 @@
+pessoas = {
+    "pessoa1": {
+        'nome: ' 'Davi Matheus',
+        'idade: ' '21',
+        'gênero: ' 'M'
+        'profissão: ' 'Backend Developer'
+    },
+    "pessoa2": {
+        'nome: ' 'Álvaro',
+        'idade: ' '21',
+        'gênero: ' 'M'
+        'profissão: ' 'QA Developer'
+    },
+    "pessoa3": {
+        'nome: ' 'Thiago',
+        'idade: ' '20',
+        'gênero: ' 'M'
+        'profissão: ' 'Frontend Developer'
+    },
+    "pessoa4": {
+        'nome: ' 'Guilherme',
+        'idade: ' '23',
+        'gênero: ' 'M'
+        'profissão: ' 'Student'
+    },
+    "pessoa5": {
+        'nome: ' 'Caetano',
+        'idade: ' '32',
+        'gênero: ' 'M'
+        'profissão: ' 'Fullstack Developer'
+    },
+    "pessoa6": {
+        'nome: ' 'Enzo',
+        'idade: ' '20',
+        'gênero: ' 'M'
+        'profissão: ' 'Student'
+    },
+}
+
+print(pessoas['pessoa1']) 
