f9d3c2e5ee84abfc8b80e866cba7dec3557c2c8f
mahtsales <davimatheusfsales@gmail.com>
2026-03-28 09:16:03 -0400

    feat: add comments about the types of list

--- class-2026-03-28/exercise-1.py
+++ class-2026-03-28/exercise-1.py
@@ -1,14 +1,16 @@
-pessoas = {
+pessoas = { #nesse set(lista), nós especificamos de maneira detalhada o que desejamos.
     "Nome" : 'Matheus',
     "Idade" : 21,
     "Cidade" : 'Manaus'
 }
-
 print(pessoas)
 
-nomes = ['Matheus', 'João', 'Maria']
+nomes = ['Matheus', 'João', 'Maria'] #set(lista) somente de nomes
 print(nomes)
 print(nomes[0])
 
-nomes.append(1, 'Davi')
+nomes.append('Davi') #.append => add um item no fim da lista
+print(nomes)
+
+nomes.insert(1, 'Alvaro') #.insert => add um item na posicao especifica
 print(nomes)
