537a82a6438d267ceeebfc46ab54bdc5e62466fd
mahtsales <davimatheusfsales@gmail.com>
2026-03-28 09:06:17 -0400

    feat: add set exercises

---
+++ class-2026-03-28/exercise-1.py
@@ -0,0 +1,14 @@
+pessoas = {
+    "Nome" : 'Matheus',
+    "Idade" : 21,
+    "Cidade" : 'Manaus'
+}
+
+print(pessoas)
+
+nomes = ['Matheus', 'João', 'Maria']
+print(nomes)
+print(nomes[0])
+
+nomes.append(1, 'Davi')
+print(nomes)
