48854d27b1903bea0eb0f589c9913126d6dabbb8
mahtsales <davimatheusfsales@gmail.com>
2026-03-28 10:08:49 -0400

    feat: add comments about variables in python

--- list-of-exercise.py
+++ list-of-exercise.py
@@ -1,11 +1,11 @@
 #criem uma lista com 5 saldos de conta corrente e ordene: maior saldo, menor saldo, media dos saldos, e o total dos saldos.
 saldos = [1000.50, 2500.75, 500.00, 1500.25, 3000.00]
-maior_saldo = max(saldos) 
-menor_saldo = min(saldos) 
-media_saldos = sum(saldos) / len(saldos)
-total_saldos = sum(saldos)
+maior_saldo = max(saldos) # retorna o maior valor
+menor_saldo = min(saldos) # retorna o menor valor
+media_saldos = sum(saldos) / len(saldos) # retorna a media dos valores existentes
+total_saldos = sum(saldos) # retorna o total dos valores
 print(f"Maior saldo: R${maior_saldo:.2f}")
 print(f"Menor saldo: R${menor_saldo:.2f}")
 print(f"Média dos saldos: R${media_saldos:.2f}")
 print(f"Total dos saldos: R${total_saldos:.2f}")
-
+#OBS.: o ':.2f' gera as duas casas decimais após o ponto.
